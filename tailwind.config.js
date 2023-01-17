34bfc482b0bbe2e16c5ec47f204bcccf8a6897bb
Jeremy Kale Padot <jeremy@padot.us>
2023-01-17 14:03:00 -0700

    aspect ration for wide and tall screens to consume viewport

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        widescreen: { raw: "(min-aspect-ration: 3/2)" },
-        tallscreen: { raw: "(min-aspect-ration: 1/2)" },
+        widescreen: { raw: "(min-aspect-ratio: 3/2)" },
+        tallscreen: { raw: "(min-aspect-ratio: 1/2)" },
       },
     },
   },
