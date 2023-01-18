f78a83849207a116be30da59aaeaebf92c0a17dc
Jeremy Kale Padot <jeremy@padot.us>
2023-01-18 16:35:18 -0700

    event listener

--- build/js/main.js
+++ build/js/main.js
@@ -5,4 +5,8 @@ const initApp = () => {
     mobileMenu.classList.toggle("hidden");
     mobileMenu.classList.toggle("flex");
   };
+  hamburgerBtn.addEventListener("click", toggleMenu);
+  mobileMenu.addEventListener("click", toggleMenu);
 };
+
+document.addEventListener("DOMContentLoaded", initApp);
